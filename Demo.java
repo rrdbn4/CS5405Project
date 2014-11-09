a626a176fdeedcbc38474489013c5707a5747fd5
mdlkwd <mdlkwd@mst.edu>
2014-11-09 16:24:32 -0600

    HeapSort + SelectionSort commit
    -Demo.java: Executes program as an Applet or Application.
    -Driver.java: Where the magic happens.
    -HeapSort.java: Visualised heap sort implementation.
    -SelectionSort.java: Visualised selection sort implementation.
    -Clock.java: One of Sabharwal's examples. He requires a digital clock in
    one of the drop down menus.

--- Demo.java
+++ Demo.java
@@ -1,1 +1,14 @@
-placeholder text
+import javax.swing.JApplet;
+
+public class Demo extends JApplet
+{
+	public void init()
+	{
+		new Driver();
+	}
+	
+	public static void main(String [] args)
+	{
+		new Driver();
+	}
+}

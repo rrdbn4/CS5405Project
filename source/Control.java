bd632f23e9906d379239ccf6e4eb858289bc95b4
hmbwbc <hmbwbc@mst.edu>
2014-11-30 19:06:13 -0600

    Update Control.java

--- source/Control.java
+++ source/Control.java
@@ -5,10 +5,16 @@ Global static constants to be used throughout the application
 */
 public class Control 
 {
+   /** MAX_SPEED is the maximum speed for the sorting algorithms to run. */
    public static int MAX_SPEED = 500;
+   /** MIN_SPEED is the minimum speed for the sorting algorithms to run. */
    public static int MIN_SPEED = 1;
+   /** DEFAULT_SPEED is the initial speed for the sorting algorithms. */
    public static int DEFAULT_SPEED = 50;
+   /** MAX_NUM_OF_ELEMENTS is the maximum amount of elements to sort. */
    public static int MAX_NUM_OF_ELEMENTS = 600;
+   /** MIN_NUM_OF_ELEMENTS is the minimum number of elements to sort. */
    public static int MIN_NUM_OF_ELEMENTS = 10;
+   /** DEFUALT_NUM_OF_ELEMENTS is the initial number of elements to sort. */
    public static int DEFUALT_NUM_OF_ELEMENTS = 30;
 }

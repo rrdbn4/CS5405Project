e0f18a97028e64de7326bcf7e527a35240b1549b
mdlkwd <mdlkwd@mst.edu>
2014-11-30 19:10:59 -0600

    JavaDoc for clock

--- source/Clock.java
+++ source/Clock.java
@@ -1,3 +1,9 @@
+/**
+ * @author Matthew Lindner 
+ * @version 1.0
+ * Class that acts as a clock. Updates every second, writing the time to a JLablel
+ */
+
 package code;
 
 import java.util.Date;
@@ -6,12 +12,19 @@ import javax.swing.JLabel;
 
 public class Clock extends JLabel implements Runnable
 {
+	/**
+	 * Constructor for clock. Creates a fixed thread pool of size 1, and immediatly executes that thread.
+	 */
 	public Clock()
 	{
 		Executor executor = Executors.newFixedThreadPool(1);
 		executor.execute(this);
 	}
 	
+	/**
+	 * Function called by execute(), runs in a thraed.
+	 * Every second, updates the JLabel with the new time
+	 */
 	public void run()
 	{
 		while(true)
@@ -19,6 +32,9 @@ public class Clock extends JLabel implements Runnable
 			setText(new Date().toString());
 			try
 			{
+				/**
+				 * Wait 1 second
+				 */
 				Thread.sleep(100);
 			}
 			catch(InterruptedException e){}
